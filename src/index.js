9ba9ec87b9335a8fb324da34db568abfbeaf27c6
github username <github email address>
2020-03-26 15:07:15 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let tasks = document.getElementById("tasks");
+});
+
+let submitBtn = document.querySelector("#submitBtn").addEventListener("click", function(event){
+  let description = document.getElementById("new-task-description").value;
+  tasks.innerHTML += `<li>${description}</li>`;
+  event.preventDefault();
 });
