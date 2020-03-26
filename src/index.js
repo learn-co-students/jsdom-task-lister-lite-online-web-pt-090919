803ad24cdafbfc022f21444660c2702c8575fe16
Aschonely <aschonely@gmail.com>
2020-03-26 14:58:35 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
+let tasks = []
+
+const list = document.getElementById('tasks');
+
+let li;
+
+
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  document.getElementById('create-task-form').addEventListener('submit', function (event) { 
+    event.preventDefault();
+    let formInput = document.getElementById('new-task-description');
+    console.log("formInput", formInput.value)
+    tasks.push(formInput.value);
+      
+      li = document.createElement("li");
+      li.innerHTML = formInput.value;
+      list.appendChild(li);
+      formInput.value = '';  
+    });
 });
+
