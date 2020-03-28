d602f34a7609a7aa0d66467304f65438dddfb656
Sharon Watkins <swatkins876@gmail.com>
2020-03-28 12:51:17 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
-document.addEventListener("DOMContentLoaded", () => {
+const taskForm = document.querySelector("#create-task-form")
   // your code here
-});
+const taskList = document.querySelector("#tasks")
+
+taskForm.addEventListener("submit", function(e) {
+  e.preventDefault()
+  const newTask = document.querySelector("#new-task-description").value
+  taskList.innerHTML += `
+    <li>
+    ${newTask}
+    <button data-action="delete"> X </button>
+     </li>
+  `
+  // let priority = document.querySelector("select option").value
+  // if (priority == "high") { taskList.innerHTML.style.backgroundColor = "red" }
+  // if (priority == "medium") { taskList.innerHTML.style.backgroundColor = "yellow" }
+  // else {taskList.innerHTML.style.backgroundC = "green"}
+  taskForm.reset()
+});	});
+
+
+taskList.addEventListener("click", function(e) {
+  if (e.target.dataset.action === "delete") {
+    e.target.parentElement.remove()
+  }
+})
