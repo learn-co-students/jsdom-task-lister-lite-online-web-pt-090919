caccb4d05c8509706ffe9fc2e6d114cf2031921c
Emil Andreasyan <emil.andreasyan@yahoo.com>
2020-03-28 06:06:48 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const taskList = new TaskList();
+  
   const createTaskForm = document.getElementById('create-task-form');
   const newTaskDescription = document.getElementById('new-task-description');
+  const newTaskPriority = document.getElementById("new-task-priority");
   const list = document.getElementById('list');
+  const tasks = document.getElementById('tasks');
+  
+  const renderApp = () => (taskUl.innerHTML = taskList.renderTasks());
   
   createTaskForm.addEventListener('submit', (e) => {
     e.preventDefault();
-    list.appendChild(newTaskDescription.value);
+    taskList.createNewTask(newTaskDescription.value);
+    e.target.reset();
+    renderApp()
   });
-  return list
+  tasks.addEventListener('click', (e) => {
+    if (e.target.nodeName === "BUTTON") {
+      taskList.deleteTask(e.target.dataset.description);
+      renderApp()
+    }
+  })
 });
