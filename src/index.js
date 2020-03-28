2cecab8d123c772f234267e304dd7e0fc6a9bef8
Aschonely <aschonely@gmail.com>
2020-03-28 11:56:20 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,21 +1,26 @@
-let tasks = []
-
-const list = document.getElementById('tasks');
-
-let li;
-
-
 document.addEventListener("DOMContentLoaded", () => {
-  document.getElementById('create-task-form').addEventListener('submit', function (event) { 
-    event.preventDefault();
-    let formInput = document.getElementById('new-task-description');
-    console.log("formInput", formInput.value)
-    tasks.push(formInput.value);
-      
-      li = document.createElement("li");
-      li.innerHTML = formInput.value;
-      list.appendChild(li);
-      formInput.value = '';  
-    });
+
+  const newTaskForm = document.getElementById("create-task-form");
+  const newTaskDescription = document.getElementById("new-task-description");
+  const newTaskPriority = document.getElementById("new-task-priority");
+
+  const newTaskUl = document.getElementById("tasks");
+
+  
+  newTaskForm.addEventListener("submit", createNewTask);
 });
 
+const createNewTask = event => {
+  event.preventDefault();
+ 
+  const newTaskDescription = document.getElementById("new-task-description");
+  const newTask = document.createElement("li");
+  newTask.innerText = newTaskDescription.value;
+
+  appendNewTask(newTask);
+  event.target.reset();
+};
+
+const appendNewTask = task => {
+  document.getElementById("tasks").appendChild(task);
+};  
