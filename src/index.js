bcd1a40800c98b6a2cd8be649b947bcee1231f97
Emil Andreasyan <emil.andreasyan@yahoo.com>
2020-03-28 04:52:52 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const createTaskForm = document.getElementById('create-task-form');
+  const newTaskDescription = document.getElementById('new-task-description');
+  const list = document.getElementById('list');
+  
+  createTaskForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    list.appendChild(newTaskDescription.value);
+  });
+  return list
 });
