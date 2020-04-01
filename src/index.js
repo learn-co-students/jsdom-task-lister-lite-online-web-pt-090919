07f7c7fe7f63a60d34e5bc86882ef1b5ab404116
jaredjames2020 <jaredjames2020@gmail.com>
2020-03-31 20:06:54 -0400

    task added to DOM after submit button

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+// document.addEventListener("DOMContentLoaded", () => {
+//   // your code here
+//   // console.log("Great");
+// });
+  const main = document.querySelector("#main-content")
+  console.log(main)
+
+  const form = document.querySelector("#create-task-form")
+  const taskList = document.querySelector("#tasks")
+
+  form.addEventListener("submit", function(e) {
+    e.preventDefault()
+    const inputForForm = document.querySelector("#new-task-description").value
+    const taskItem = document.createElement("li")
+    taskItem.innerText = inputForForm
+    taskList.appendChild(taskItem)
+    form.reset()
+  })
