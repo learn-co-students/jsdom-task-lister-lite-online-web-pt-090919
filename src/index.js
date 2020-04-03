b038321baf749fa0f4d1f8942d24c3d2ae1731c5
kalmegati <kalmegati@gmail.com>
2020-04-03 17:52:56 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const tasks = document.getElementById("tasks");
+  stopper();
 });
+
+function stopper(){
+  document.getElementsByTagName("input")[1].addEventListener("click", function(event) {
+    console.log("meep");
+    event.preventDefault();
+    newItem();
+  }, false);
+}
+
+function newItem() {
+  let item = document.getElementsByTagName("input")[0].value;
+  let li = document.createElement("li");
+  li.textContent = item;
+  tasks.appendChild(li);
+};
+
